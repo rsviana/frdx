971031deef39963afcf9009091e36099da51cb7b
rsviana <rsviana@gmail.com>
2026-01-30 07:42:22 -0300

    Corrigindo Tests

--- tests/test_dns.py
+++ tests/test_dns.py
@@ -1,8 +1,9 @@
+import pytest
 from frd.dns.resolve import resolve
 
+
+@pytest.mark.integration
 def test_dns_resolve_a():
-    # teste "real" depende de rede; marcamos como opcional
-    # rode com: pytest -q -k dns --maxfail=1
     res = resolve("example.com", "A")
     assert len(res) >= 1
     assert res[0]["type"] == "A"
